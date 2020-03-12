0300ee72486790bd35ce72cf201901dbeb47d68a
Rickson <rickson.simoes@hotmail.com>
2020-03-12 12:17:58 -0300

    :art: :hammer:  :pencil: Edicao e Css

--- pages/home.js
+++ pages/home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Corpo, Teste } from '../styles/style';
 
 import Link from 'next/link';
 import Head from 'next/head';
--- styles/style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Corpo = styled.body`
-        margin: 0;
-        background: red;
-`;
